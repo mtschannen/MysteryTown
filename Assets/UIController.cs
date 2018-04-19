9a40e80bca841dd682b921717e1c7e5cc425a2ce
Unknown <tschannenmatt@gmail.com>
2018-04-19 18:55:42 -0500

    Small adjustments

---
+++ Assets/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+	public GameObject startCanvas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space"))
+			startCanvas.SetActive(false);
+		if (Input.GetKeyDown("escape"))
+			Application.Quit();
+	}
+}
