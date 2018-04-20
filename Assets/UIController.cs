f3cc396234301e8eabe90e7b177c3129f49a4482
Unknown <tschannenmatt@gmail.com>
2018-04-20 01:35:18 -0500

    floors and better startpoint

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class UIController : MonoBehaviour {
 
 	public GameObject startCanvas;
+	public GameObject firstPersonController;
 	// Use this for initialization
 	void Start () {
-		
+		firstPersonController.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("space"))
-			startCanvas.SetActive(false);
+		if (Input.GetKeyDown ("space")) {
+			startCanvas.SetActive (false);
+			firstPersonController.SetActive (true);
+		}
 		if (Input.GetKeyDown("escape"))
 			Application.Quit();
 	}
