b4b181e7a8f0b0938a8f213640cb05e46328b8e5
Unknown <tschannenmatt@gmail.com>
2018-05-10 22:39:59 -0500

    Auto stash before merge of "master" and "origin/master"

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -15,19 +15,20 @@ public class UIController : MonoBehaviour {
 	public GameObject objective1;
 	public GameObject objective2;
 	public GameObject objective3;
-//	public GameObject objective4;
-//	public GameObject objective5;
-//	public GameObject objective6;
-//	public GameObject objective7;
+	public GameObject objective4;
+	public GameObject objective5;
+	public GameObject objective6;
+	public GameObject objective7;
 	public GameObject objective1Loaded;
 	public GameObject objective2Loaded;
 	public GameObject objective3Loaded;
-	//	public GameObject objective4Loaded;
-	//	public GameObject objective5Loaded;
-	//	public GameObject objective6Loaded;
-	//	public GameObject objective7Loaded;
+	public GameObject objective4Loaded;
+	public GameObject objective5Loaded;
+	public GameObject objective6Loaded;
+	public GameObject objective7Loaded;
 	public GameObject dialogueBox;
 	public GameObject progressBox;
+	public GameObject endGameBox;
 	public Text dialogueText;
 	public Text progressText;
 
@@ -46,7 +47,7 @@ public class UIController : MonoBehaviour {
 		if (objectiveCount < 7) {
 			progressText.text = "Supplies needed: " + (7 - objectiveCount).ToString ();
 		} else {
-			progressText.text = "You have enough supplies, get to the boat!";
+			progressText.text = "Get to the boat!";
 		}
 		if (Input.GetKeyDown ("space")) {
 			startCanvas.SetActive (false);
@@ -74,17 +75,18 @@ public class UIController : MonoBehaviour {
             float dist1 = Vector3.Distance(firstPersonCamera.transform.position, objective1.transform.position);
 			float dist2 = Vector3.Distance(firstPersonCamera.transform.position, objective2.transform.position);
 			float dist3 = Vector3.Distance(firstPersonCamera.transform.position, objective3.transform.position);
-//			float dist4 = Vector3.Distance(firstPersonCamera.transform.position, objective4.transform.position);
-//			float dist5 = Vector3.Distance(firstPersonCamera.transform.position, objective5.transform.position);
-//			float dist6 = Vector3.Distance(firstPersonCamera.transform.position, objective6.transform.position);
-//			float dist7 = Vector3.Distance(firstPersonCamera.transform.position, objective7.transform.position);
+			float dist4 = Vector3.Distance(firstPersonCamera.transform.position, objective4.transform.position);
+			float dist5 = Vector3.Distance(firstPersonCamera.transform.position, objective5.transform.position);
+			float dist6 = Vector3.Distance(firstPersonCamera.transform.position, objective6.transform.position);
+			float dist7 = Vector3.Distance(firstPersonCamera.transform.position, objective7.transform.position);
 			if(distBoat <= 10){
 				dialogueBox.SetActive(true);
 				dialogueText.text = "You must collect more resources before you can leave!";
 				if (objectiveCount >= 7) {
 					dialogueText.text = "You have enough resources, press E to cast off!";
 					if (Input.GetKeyDown ("e")) {
-						//player wins
+						endGameBox.SetActive (true);
+						firstPersonController.SetActive (false);
 					}
 				}
 			}
@@ -125,22 +127,54 @@ public class UIController : MonoBehaviour {
 					}
 				}
 			}
-//			else if(dist4 <= 25){
-//				dialogueBox.SetActive(true);
-//				dialogueText.text = "Objective 4 Nearby";
-//			}
-//			else if(dist5 <= 25){
-//				dialogueBox.SetActive(true);
-//				dialogueText.text = "Objective 5 Nearby";
-//			}
-//			else if(dist6 <= 25){
-//				dialogueBox.SetActive(true);
-//				dialogueText.text = "Objective 6 Nearby";
-//			}
-//			else if(dist7 <= 25){
-//				dialogueBox.SetActive(true);
-//				dialogueText.text = "Objective 7 Nearby";
-//			}
+			else if(dist4 <= 15 && objective4.activeInHierarchy == true){
+				dialogueBox.SetActive(true);
+				dialogueText.text = "Supplies nearby.";
+				if (dist4 <= 2.5) {
+					dialogueText.text = "Press E to collect resource.";
+					if (Input.GetKeyDown ("e")) {
+						objective4.SetActive (false);
+						objective4Loaded.SetActive (true);
+						objectiveCount++;
+					}
+				}
+			}
+			else if(dist5 <= 15 && objective5.activeInHierarchy == true){
+				dialogueBox.SetActive(true);
+				dialogueText.text = "Supplies nearby.";
+				if (dist5 <= 2.5) {
+					dialogueText.text = "Press E to collect resource.";
+					if (Input.GetKeyDown ("e")) {
+						objective5.SetActive (false);
+						objective5Loaded.SetActive (true);
+						objectiveCount++;
+					}
+				}
+			}
+			else if(dist6 <= 15 && objective6.activeInHierarchy == true){
+				dialogueBox.SetActive(true);
+				dialogueText.text = "Supplies nearby.";
+				if (dist6 <= 2.5) {
+					dialogueText.text = "Press E to collect resource.";
+					if (Input.GetKeyDown ("e")) {
+						objective6.SetActive (false);
+						objective6Loaded.SetActive (true);
+						objectiveCount++;
+					}
+				}
+			}
+			else if(dist7 <= 15 && objective7.activeInHierarchy == true){
+				dialogueBox.SetActive(true);
+				dialogueText.text = "Supplies nearby.";
+				if (dist7 <= 2.5) {
+					dialogueText.text = "Press E to collect resource.";
+					if (Input.GetKeyDown ("e")) {
+						objective7.SetActive (false);
+						objective7Loaded.SetActive (true);
+						objectiveCount++;
+					}
+				}
+			}
             else
             {
 				dialogueBox.SetActive(false);
